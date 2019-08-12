25d545fe975241a44f3682cd1d8e208f131f3827
Eduardo <eduardosantin38@gmail.com>
2019-08-11 21:34:14 -0300

    login no firebase

--- src/pages/login-usuario/login-usuario.ts
+++ src/pages/login-usuario/login-usuario.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AlertController } from 'ionic-angular';
+
 
 @IonicPage({
   name: "login-usuario"
@@ -13,15 +16,41 @@ export class LoginUsuarioPage {
 
   loginForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formbuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public formbuilder: FormBuilder,
+    public afAuth: AngularFireAuth,
+    public alertCtrl: AlertController) {
     this.loginForm = this.formbuilder.group({
       email: [null, [Validators.required, Validators.email]],
       senha: [null, [Validators.required, Validators.minLength(6)]]
     })
   }
   
-  submitForm(){
-    console.log(this.loginForm.value);
+  submitLogin(){
+    this.afAuth.auth.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.senha)
+    .then(() => {
+      this.presentAlert("Logado com Sucesso", "");
+      this.navCtrl.setRoot("start-page");
+    }). catch((error) => {
+      if(error.code == 'auth/wrong-password'){
+        this.presentAlert("Erro", "Senha incorreta, digite novamente");
+        this.loginForm.controls['senha'].setValue(null);
+      }
+      if(error.code == 'auth/user-not-found'){
+        this.presentAlert("Erro", "Usuario não encontrado");
+        this.loginForm.controls['email'].setValue(null);
+        this.loginForm.controls['senha'].setValue(null);
+      }
+    })
   }
 
+  presentAlert(title: string, subTitle: string){
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subTitle,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
