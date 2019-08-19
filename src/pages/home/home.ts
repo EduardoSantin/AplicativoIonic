91b3097be28df1ce6cd7c2e843eac44c4628c807
Eduardo <eduardosantin38@gmail.com>
2019-08-18 23:00:52 -0300

    criado funcao deslogar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
+
+// @IonicPage({
+//   name: 'page-home'
+// })
 
 @Component({
   selector: 'page-home',
--- src/pages/start/start.ts
+++ src/pages/start/start.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @IonicPage({
   name: 'start-page'
@@ -13,7 +14,14 @@ export class StartPage {
   uid: string;
 
   constructor(public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public navParams: NavParams,
+    public storage: Storage) {
+  }
+
+  sair(){
+    this.storage.remove("user");
+    console.log("usuario deslogado"); 
+    this.navCtrl.setRoot("login-usuario");
   }
 
 }
