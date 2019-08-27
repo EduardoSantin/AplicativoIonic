29b6a222b1a198cad27352c5d3dd60494fe2e368
Eduardo <eduardosantin38@gmail.com>
2019-08-26 21:55:02 -0300

    adicionado tela de nova area

---
+++ src/pages/nova-area/nova-area.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { NovaAreaPage } from './nova-area';
+
+@NgModule({
+  declarations: [
+    NovaAreaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(NovaAreaPage),
+  ],
+})
+export class NovaAreaPageModule {}
---
+++ src/pages/nova-area/nova-area.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage({
+  name: "nova-area"
+})
+@Component({
+  selector: 'page-nova-area',
+  templateUrl: 'nova-area.html',
+})
+export class NovaAreaPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+}
