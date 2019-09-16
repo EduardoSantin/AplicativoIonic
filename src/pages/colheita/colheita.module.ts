4d3bf9831ee3ca991d8875a7e82de7ee34533250
Eduardo <eduardosantin38@gmail.com>
2019-09-15 22:30:46 -0300

    iniciado colheita da area plantada

---
+++ src/pages/colheita/colheita.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ColheitaPage } from './colheita';
+
+@NgModule({
+  declarations: [
+    ColheitaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ColheitaPage),
+  ],
+})
+export class ColheitaPageModule {}
---
+++ src/pages/colheita/colheita.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { Http } from '@angular/http';
+
+@IonicPage({
+  name: "colheita"
+})
+@Component({
+  selector: 'page-colheita',
+  templateUrl: 'colheita.html',
+})
+export class ColheitaPage {
+
+  uid;
+  item;
+  list;
+
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: Storage,
+    public http: Http) {
+  }
+  
+  salvaColheita(item, sacas, dataColheita){
+
+  }
+
+  getList(){
+    console.log(this.item);
+    this.http.get('https://fir-login-26b40.firebaseio.com/plantio/'+this.uid+'/'+this.item.key+'.json')
+    .map(res => res.json())
+    .subscribe(data => {
+      if(data != null && data != undefined){
+        this.trataDados(data);
+      }
+    })
+  }
+
+  trataDados(dados){
+    if(dados!= null){
+      this.list = Object.keys(dados).map(i => {
+        dados[i].key = i;
+        return dados[i];
+      });
+    }
+  }
+
+
+  ionViewDidLoad() {
+    this.storage.get("user").then((resolve) => {
+      this.uid = resolve;
+    })
+    this.storage.get("item").then((resolve) => {
+      this.item = resolve;
+      this.getList();
+    })
+  }
+
+}
